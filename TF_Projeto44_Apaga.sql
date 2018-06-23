7413553a80f57be91ff7787cc1563d77748c6ee8
Thiago Nogueira <thiagonf10@gmail.com>
2018-06-23 18:01:56 -0300

    Adicionando scrip de apagar tabelas do banco

---
+++ TF_Projeto44_Apaga.sql
@@ -0,0 +1,18 @@
+-- ------------------------------------------------------
+-- Projeto Final - Banco MySQL
+-- Aluno: Thiago Nogueira Freire
+-- Matricula: 15/0047142
+-- Base de Dados = TF_PRONATEC
+--   01 = Base de Dados
+--   04 = Tabelas
+-- ------------------------------------------------------
+
+USE TF_PRONATEC;
+
+drop table dados_errados;
+
+drop table matriculados;
+
+drop table concluintes;
+
+drop table unidades;
